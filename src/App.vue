dc3f4d1a94adcbd14c7dfdbfe8ea183e31f96efd
IlYess <najiilyassoo@gmail.com>
2023-11-25 04:05:35 +0100

    Enhance application styling: Add additional CSS styles

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
   <router-view/>
 </template>
 
-<style>
 
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #EDEDED;
+}
 </style>
