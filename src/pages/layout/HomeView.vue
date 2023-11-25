f99eb00c84e7b6f729cf3d6b4e337e511fa7d04b
IlYess <najiilyassoo@gmail.com>
2023-11-25 03:56:39 +0100

    Add second component to HomePage

--- src/pages/layout/HomeView.vue
+++ src/pages/layout/HomeView.vue
@@ -2,17 +2,19 @@
   <div class="home">
       <NavBar/>
       <IntroHome/>
+      <ChiffresHome/>
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/componentsHome/NavBar.vue';
 import IntroHome from '@/components/componentsHome/IntroHome.vue';
+import ChiffresHome from '@/components/componentsHome/ChiffresHome.vue';
 
 
 export default {
   name: 'HomeView',
-  components: {NavBar, IntroHome},
+  components: {NavBar, IntroHome, ChiffresHome},
   
 }
 </script>
