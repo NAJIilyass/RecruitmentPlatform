9057d61c1fdb7700fe429ac6b12362092ac0c000
IlYess <najiilyassoo@gmail.com>
2023-11-25 04:00:06 +0100

    Add third component to HomePage

---
+++ src/data/data.js
@@ -0,0 +1,45 @@
+import pop1 from "/src/assets/Popular/Img1.png";
+import pop2 from "/src/assets/Popular/Img2.png";
+import pop3 from "/src/assets/Popular/Img3.png";
+import pop4 from "/src/assets/Popular/Img4.jpg";
+import pop5 from "/src/assets/Popular/Img5.jpg";
+import pop6 from "/src/assets/Popular/Img6.jpg";
+
+export const services = [
+    {
+        id : 1,
+        icon : pop1,
+        quote : "Live your dreams",
+        title : "Web Development"
+    },
+    {
+        id : 2,
+        icon : pop2,
+        quote : "Engage your audience",
+        title : "Video Explainer"
+    },
+    {
+        id : 3,
+        icon : pop3,
+        quote : "Share your message",
+        title : "Voice Over"
+    },
+    {
+        id : 4,
+        icon : pop4,
+        quote : "Build your brand",
+        title : "Responsive Design"
+    },
+    {
+        id : 5,
+        icon : pop5,
+        quote : "Live your dreams",
+        title : "Car Driver"
+    },
+    {
+        id : 6,
+        icon : pop6,
+        quote : "Live your dreams",
+        title : "Truck Driver"
+    }
+];
--- src/pages/layout/HomeView.vue
+++ src/pages/layout/HomeView.vue
@@ -3,6 +3,7 @@
       <NavBar/>
       <IntroHome/>
       <ChiffresHome/>
+      <PopularHome/>
   </div>
 </template>
 
@@ -10,11 +11,12 @@
 import NavBar from '@/components/componentsHome/NavBar.vue';
 import IntroHome from '@/components/componentsHome/IntroHome.vue';
 import ChiffresHome from '@/components/componentsHome/ChiffresHome.vue';
+import PopularHome from '@/components/componentsHome/PopularHome.vue';
 
 
 export default {
   name: 'HomeView',
-  components: {NavBar, IntroHome, ChiffresHome},
+  components: {NavBar, IntroHome, ChiffresHome, PopularHome},
   
 }
 </script>
