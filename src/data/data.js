42568af70335144f4bd7fc5bde4ca0e3fbc2f86c
IlYess <najiilyassoo@gmail.com>
2023-11-26 03:28:43 +0100

    Include two components on the HomePage and make modifications to the data file

--- src/data/data.js
+++ src/data/data.js
@@ -5,6 +5,16 @@ import pop4 from "/src/assets/Popular/Img4.jpg";
 import pop5 from "/src/assets/Popular/Img5.jpg";
 import pop6 from "/src/assets/Popular/Img6.jpg";
 
+import som1 from "/src/assets/Something/Img1.png";
+import som2 from "/src/assets/Something/Img2.png";
+import som3 from "/src/assets/Something/Img3.png";
+import som4 from "/src/assets/Something/Img4.png";
+
+import great1 from "/src/assets/great/Ima1.png";
+import great2 from "/src/assets/great/Ima2.png";
+import great3 from "/src/assets/great/Ima3.png";
+import great4 from "/src/assets/great/Ima4.png";
+
 export const services = [
     {
         id : 1,
@@ -41,5 +51,59 @@ export const services = [
         icon : pop6,
         quote : "Live your dreams",
         title : "Truck Driver"
+    }
+];
+
+export const steps = [
+    {
+        id : 1,
+        icon : som1,
+        title : "Post a job",
+        desc : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor."
+    },
+    {
+        id : 2,
+        icon : som2,
+        title : "Choose freelancers",
+        desc : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor."
+    },
+    {
+        id : 3,
+        icon : som3,
+        title : "Pay safely",
+        desc : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor."
+    },
+    {
+        id : 4,
+        icon : som4,
+        title : "We're here to help",
+        desc : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor."
+    }
+];
+
+export const qualities = [
+    {
+        id : 1,
+        icon : great1,
+        title : "Browse portfolios",
+        desc : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor."
+    },
+    {
+        id : 2,
+        icon : great2,
+        title : "Fast bids",
+        desc : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor."
+    },
+    {
+        id : 3,
+        icon : great3,
+        title : "Quality work",
+        desc : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor."
+    },
+    {
+        id : 4,
+        icon : great4,
+        title : "Track progress",
+        desc : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor."
     }
 ];
--- src/pages/layout/HomeView.vue
+++ src/pages/layout/HomeView.vue
@@ -4,6 +4,8 @@
       <IntroHome/>
       <ChiffresHome/>
       <PopularHome/>
+      <NeedSomething/>
+      <WhatGreat/>
   </div>
 </template>
 
@@ -12,11 +14,13 @@ import NavBar from '@/components/componentsHome/NavBar.vue';
 import IntroHome from '@/components/componentsHome/IntroHome.vue';
 import ChiffresHome from '@/components/componentsHome/ChiffresHome.vue';
 import PopularHome from '@/components/componentsHome/PopularHome.vue';
+import NeedSomething from '@/components/componentsHome/NeedSomething.vue';
+import WhatGreat from '@/components/componentsHome/WhatGreat.vue';
 
 
 export default {
   name: 'HomeView',
-  components: {NavBar, IntroHome, ChiffresHome, PopularHome},
+  components: {NavBar, IntroHome, ChiffresHome, PopularHome, NeedSomething, WhatGreat},
   
 }
 </script>
