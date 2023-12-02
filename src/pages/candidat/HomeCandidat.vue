5b7554859f761ff4db12d039bea54a0178882e85
IlYess <najiilyassoo@gmail.com>
2023-12-02 08:04:10 +0100

    Set up and added Sidebar and Navbar to candidate's homepage

--- src/pages/candidat/HomeCandidat.vue
+++ src/pages/candidat/HomeCandidat.vue
@@ -1,13 +1,34 @@
 <template>
+    <div class="w-screen h-full relative font-montserrat">
+        <sideBar class="fixed left-0" :sideOpen="sideOpen"/>
+        
+    </div>
   
 </template>
 
 <script>
-export default {
+import sideBar from '@/components/componentsCandidat/sideBar.vue'
 
+import { ref } from 'vue'
+
+export default {
+    components: {sideBar},
+    setup(){
+        const sideOpen = ref(true)
+        const clickSide = () => {
+            sideOpen.value = !sideOpen.value
+        }
+
+        const id1 = ref(0)
+        const change = (id) => {
+            if(id1.value==id){
+                id1.value = 0
+            }else{
+                id1.value = id
+            } 
+            console.log(id1.value)          
+        }
+        return {sideOpen, clickSide, id1, change}
+    }
 }
 </script>
-
-<style>
-
-</style>
