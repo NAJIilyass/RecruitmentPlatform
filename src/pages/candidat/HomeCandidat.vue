b8edda85533ad3910c8f5a1e501b531b4a4a3485
IlYess <najiilyassoo@gmail.com>
2023-12-03 06:05:57 +0100

    Set up Navbar for candidate's homepage

--- src/pages/candidat/HomeCandidat.vue
+++ src/pages/candidat/HomeCandidat.vue
@@ -1,18 +1,25 @@
 <template>
     <div class="w-screen h-full relative font-montserrat">
         <sideBar class="fixed left-0" :sideOpen="sideOpen"/>
-        
+        <div class="absolute right-0 overflow-x-hidden" :class="sideOpen==true ? 'w-[calc(100vw-250px)]' : 'w-[calc(100vw-70px)]'">
+            <navBar :sideOpen="sideOpen" :clickSide="clickSide" :change="change"/>
+            
+            <div class="w-full h-[calc(100vh-50px)] z-0" @click="change(0)">
+                <router-view></router-view>
+            </div>
+        </div>
     </div>
   
 </template>
 
 <script>
 import sideBar from '@/components/componentsCandidat/sideBar.vue'
+import navBar from '@/components/componentsCandidat/navBar.vue'
 
 import { ref } from 'vue'
 
 export default {
-    components: {sideBar},
+    components: {sideBar, navBar},
     setup(){
         const sideOpen = ref(true)
         const clickSide = () => {
