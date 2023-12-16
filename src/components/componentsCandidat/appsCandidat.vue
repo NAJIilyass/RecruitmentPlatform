3cfd4d7fe5c07de8b193c2014067068186527ce8
IlYess <najiilyassoo@gmail.com>
2023-12-16 04:58:36 +0100

    Adding the MyApplications component

--- src/components/componentsCandidat/appsCandidat.vue
+++ src/components/componentsCandidat/appsCandidat.vue
@@ -1,13 +1,27 @@
 <template>
+    <div class="pb-8">
+      <div class="bg-achmi-primary text-white font-semibold text-xl h-16 flex justify-center mt-12">
+        <p class="my-auto">My Applications</p>
+      </div>
+      <div>
+        <appCandidat v-for="company in companies" :key="company.id" :company="company"/>
+      </div>
+    </div>
+  </template>
   
-</template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
+  <script>
+  import { companies } from '@/data/data'
+  import appCandidat from './appCandidat.vue'
+  import { ref } from 'vue'
+  
+  export default {
+    components: {appCandidat},
+    setup(){
+      return {companies}
+    }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
